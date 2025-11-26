a44b4339457dc8be73a76d61b1d308abebe3ff3c
FedericoBustaffa <kiko.bustaffa@gmail.com>
2025-11-26 15:25:03 +0100

    fetch script

--- scripts/fetch.py
+++ scripts/fetch.py
@@ -1,19 +1,38 @@
 import argparse
+import os
 
 import pandas as pd
 from ucimlrepo import fetch_ucirepo
 
+
+def fetch(id: int):
+    if "datasets" not in os.listdir("."):
+        os.mkdir("datasets")
+
+    ds = fetch_ucirepo(id=id)
+    if ds.data is not None:
+        X = ds.data.features
+        y = ds.data.targets
+        df = pd.concat([X, y], axis=1)
+        if ds.metadata is not None:
+            name = ds.metadata.name
+            name = name.lower()
+            name = name.replace(" ", "_")
+            name = name.replace("(", "")
+            name = name.replace(")", "")
+
+            df.to_csv(f"datasets/{name}.csv", index=False, header=False)
+            print(f"{name} fetched")
+
+
 parser = argparse.ArgumentParser()
-parser.add_argument("id", type=int, help="id of the UCI dataset")
+parser.add_argument("--id", type=int, help="id of the UCI dataset")
 args = parser.parse_args()
 
+IDs = [53, 17, 159, 80, 31]
 
-dataset = fetch_ucirepo(id=args.id)
-if dataset.data is not None:
-    X = dataset.data.features
-    y = dataset.data.targets
-    df = pd.concat([X, y], axis=1)
-    if dataset.metadata is not None:
-        name = dataset.metadata.name
-        df.to_csv(f"datasets/{name.lower()}.csv", index=False, header=False)
-        print(f"{name} fetched")
+if args.id is None:
+    for id in IDs:
+        fetch(id)
+else:
+    fetch(args.id)
