24d76a6ddae1c41e969a2341debae0afc9f0e225
FedericoBustaffa <kiko.bustaffa@gmail.com>
2025-11-30 18:46:52 +0100

    scripts

--- scripts/slurm_benchmark.sh
+++ scripts/slurm_benchmark.sh
@@ -6,10 +6,10 @@ for i in 64 128 256 512; do
         echo "estimators: ${i}"
         echo "threads: ${j}"
         if [[ j -eq 1 ]]; then
-            srun -N 1 -n 1 -c $j ./build/rf_bm.out $i 0 "seq" $j $1
+            srun -N 1 -n 1 -c $j ./build/rf.out $i 0 "seq" $j $1
         else
-            srun -N 1 -n 1 -c $j ./build/rf_bm.out $i 0 "omp" $j $1
-            srun -N 1 -n 1 -c $j ./build/rf_bm.out $i 0 "ff" $j $1
+            srun -N 1 -n 1 -c $j ./build/rf.out $i 0 "omp" $j $1
+            srun -N 1 -n 1 -c $j ./build/rf.out $i 0 "ff" $j $1
         fi
         echo ""
     done
