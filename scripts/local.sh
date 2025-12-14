0e631928d13ca892fc63f315c5c5f24dfdee2fba
FedericoBustaffa <kiko.bustaffa@gmail.com>
2025-12-14 21:19:47 +0100

    res

--- scripts/local.sh
+++ scripts/local.sh
@@ -11,7 +11,7 @@ done
 
 # openmp
 for i in 32 64 128 256; do
-    for t in 1 2 4 8 16 32; do
+    for t in 1 2 4; do
         for j in $(seq 1 5); do
             ./build/rf.out $i 0 "omp" $t 1 $1 1
         done
@@ -21,7 +21,7 @@ done
 
 # fastflow
 for i in 32 64 128 256; do
-    for t in 1 2 4 8 16 32; do
+    for t in 1 2 4; do
         for j in $(seq 1 5); do
             ./build/rf.out $i 0 "ff" $t 1 $1 1
         done
@@ -31,7 +31,7 @@ done
 
 # mpi on my local machines
 for i in 32 64 128 256; do
-    for t in 1 2 4 8 16 32; do
+    for t in 1 2 4; do
         for n in 1 2; do
             for j in $(seq 1 5); do
                 mpirun -n $n --hostfile hosts.txt --bind-to none --map-by ppr:1:node ./build/rf.out $i 0 "mpi" $t $n $1 1
