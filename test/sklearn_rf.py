51f5da77b74b19e4380d9060e7a677b13dd4ea8b
FedericoBustaffa <kiko.bustaffa@gmail.com>
2025-12-26 18:13:56 +0100

    sklearn rf

--- test/sklearn_rf.py
+++ test/sklearn_rf.py
@@ -1,4 +1,5 @@
 import argparse
+import time
 
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
@@ -27,18 +28,28 @@ if __name__ == "__main__":
         max_features=None,
         n_jobs=args.njobs,
     )
-    rf.fit(X_train, y_train)
 
+    start = time.perf_counter()
+    rf.fit(X_train, y_train)
+    end = time.perf_counter()
+    print(f"train time: {end - start:.4f} s")
+
+    start = time.perf_counter_ns() * 1000
     train_pred = rf.predict(X_train)
+    end = time.perf_counter_ns() * 1000
+    print(f"train predict time: {end - start:.4f} ms")
+
     train_accuracy = accuracy_score(y_train, train_pred)
     train_f1 = f1_score(y_train, train_pred, average="macro")
-
     print(f"train accuracy: {train_accuracy:.2f}")
     print(f"train f1: {train_f1:.2f}")
 
+    start = time.perf_counter()
     test_pred = rf.predict(X_test)
+    end = time.perf_counter()
+    print(f"test predict time: {end - start:.4f} s")
+
     test_accuracy = accuracy_score(y_test, test_pred)
     test_f1 = f1_score(y_test, test_pred, average="macro")
-
     print(f"test accuracy: {test_accuracy:.2f}")
     print(f"test f1: {test_f1:.2f}")
