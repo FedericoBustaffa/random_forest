9d2727fc65c0442c6714c8ba20e51e72b73a0d8e
FedericoBustaffa <kiko.bustaffa@gmail.com>
2026-01-09 01:55:05 +0100

    sklearn decision tree test

---
+++ test/sklearn_dt.py
@@ -0,0 +1,51 @@
+import argparse
+import time
+
+import pandas as pd
+from sklearn.metrics import accuracy_score, f1_score
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("max_depth", type=int, help="max depth of trees")
+    parser.add_argument("dataset", type=str, help="dataset filepath")
+    args = parser.parse_args()
+    max_depth = args.max_depth if args.max_depth > 0 else None
+
+    df = pd.read_csv(args.dataset)
+    X = df.iloc[:, :-1].to_numpy()
+    y = df.iloc[:, -1].to_numpy()
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+    rf = DecisionTreeClassifier(
+        criterion="entropy",
+        max_depth=max_depth,
+        max_features=None,
+    )
+
+    start = time.perf_counter()
+    rf.fit(X_train, y_train)
+    end = time.perf_counter()
+    print(f"train time: {end - start:.4f} s")
+
+    start = time.perf_counter()
+    train_pred = rf.predict(X_train)
+    end = time.perf_counter()
+    print(f"train predict time: {end - start:.4f} s")
+
+    train_accuracy = accuracy_score(y_train, train_pred)
+    train_f1 = f1_score(y_train, train_pred, average="macro")
+    print(f"train accuracy: {train_accuracy:.2f}")
+    print(f"train f1: {train_f1:.2f}")
+
+    start = time.perf_counter()
+    test_pred = rf.predict(X_test)
+    end = time.perf_counter()
+    print(f"test predict time: {end - start:.4f} s")
+
+    test_accuracy = accuracy_score(y_test, test_pred)
+    test_f1 = f1_score(y_test, test_pred, average="macro")
+    print(f"test accuracy: {test_accuracy:.2f}")
+    print(f"test f1: {test_f1:.2f}")
