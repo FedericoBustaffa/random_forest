b27527492f6493bd9254a52d04f669ef47087b5a
FedericoBustaffa <kiko.bustaffa@gmail.com>
2026-01-22 18:21:19 +0100

    nodes

--- scripts/cluster.sh
+++ scripts/cluster.sh
@@ -32,7 +32,7 @@ done
 # mpi
 for i in 32 64 128; do
     for t in 16 32; do
-        for n in 1 2 4 6 8; do
+        for n in 2 4 6 8; do
             for j in $(seq 1 5); do
                 srun --mpi=pmix -N $n -n $n -c 32 ./build/rf.out $i 0 "mpi" $t $n $1 1
             done
