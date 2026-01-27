3cea9f9c95b9c17f91c5126fc8a6294cbe989d7b
FedericoBustaffa <kiko.bustaffa@gmail.com>
2026-01-27 01:52:06 +0100

    scripts

--- scripts/cluster.sh
+++ scripts/cluster.sh
@@ -36,7 +36,7 @@ for i in 32 64 128 256; do
     for t in 8 16 32; do
         for n in 2 4 6 8; do
             for j in $(seq 1 5); do
-                srun --mpi=pmix -N $n -n $n -c 32 ./build/rf.out $i 0 "mpi" $t $1 log
+                srun --mpi=pmix -N $n -n $n -c 32 ./build/rf.out $i 0 "mpi" $t $1 "log"
             done
         done
     done
