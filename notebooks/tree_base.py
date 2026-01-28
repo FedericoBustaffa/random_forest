fc27a4358ba56d677ee408233b294f0642837103
FedericoBustaffa <kiko.bustaffa@gmail.com>
2026-01-28 15:07:40 +0100

    scripts

--- notebooks/tree_base.py
+++ notebooks/tree_base.py
@@ -3,6 +3,4 @@ import pandas as pd
 df = pd.read_csv("results/tree.csv")
 
 print(df)
-
-with open("tree_table.tex", "w") as fp:
-    print(df.to_latex(index=False, float_format="%.2f"), file=fp)
+print(df.to_latex(index=False, float_format="%.2f"))
