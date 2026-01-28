1e0b3b0b59dae2b3f0fa06eea549eb0f6032b14f
FedericoBustaffa <kiko.bustaffa@gmail.com>
2026-01-28 23:00:12 +0100

    results

--- scripts/all.sh
+++ scripts/all.sh
@@ -3,5 +3,5 @@
 ./scripts/forest.sh datasets/breast_cancer.csv
 ./scripts/forest.sh datasets/magic.csv
 ./scripts/forest.sh datasets/susy20000.csv
-./scripts/forest.sh datasets/susy100000.csv
+./scripts/forest.sh datasets/susy100000.csv mpi
 
